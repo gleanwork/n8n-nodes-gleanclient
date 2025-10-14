334b9447ae72567d96089dd33ef07ee08487edac
Dejim Juang <dejimj@gmail.com>
2025-10-14 12:40:33 -0500

    Update GleanClientApi documentation URL
    Changed the documentationUrl in GleanClientApi.credentials.ts to point to the official Glean API client getting started guide. Also updated package version in package-lock.json.

--- credentials/GleanClientApi.credentials.ts
+++ credentials/GleanClientApi.credentials.ts
@@ -6,7 +6,7 @@ export class GleanClientApi implements ICredentialType {
 	// Uses the link to this tutorial as an example
 	// Replace with your own docs links when building your own nodes
 	documentationUrl =
-		'https://docs.n8n.io/integrations/creating-nodes/build/declarative-style-node/';
+		'https://developers.glean.com/api-info/client/getting-started/overview';
 	properties: INodeProperties[] = [
 		{
 			displayName: 'Base URL',
