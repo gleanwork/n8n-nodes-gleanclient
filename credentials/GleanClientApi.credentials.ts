83b283d3147e455cd42de9559b4602f045b771e9
Chris Freeman <chris.freeman@glean.com>
2025-11-20 10:49:10 -0700

    tabs -> spaces

--- credentials/GleanClientApi.credentials.ts
+++ credentials/GleanClientApi.credentials.ts
@@ -1,45 +1,45 @@
 import {
-	IAuthenticateGeneric,
-	ICredentialType,
-	INodeProperties,
-	ICredentialTestRequest,
+  IAuthenticateGeneric,
+  ICredentialType,
+  INodeProperties,
+  ICredentialTestRequest,
 } from 'n8n-workflow';
 
 export class GleanClientApi implements ICredentialType {
-	name = 'gleanClientApi';
-	displayName = 'Glean Client API';
-	// Uses the link to this tutorial as an example
-	// Replace with your own docs links when building your own nodes
-	documentationUrl = 'https://developers.glean.com/api-info/client/getting-started/overview';
-	properties: INodeProperties[] = [
-		{
-			displayName: 'Base URL',
-			name: 'baseUrl',
-			type: 'string',
-			default: 'https://support-lab-be.glean.com',
-		},
-		{
-			displayName: 'API Key',
-			name: 'apiKey',
-			type: 'string',
-			typeOptions: { password: true },
-			default: '',
-		},
-	];
-	authenticate: IAuthenticateGeneric = {
-		type: 'generic',
-		properties: {
-			headers: {
-				Authorization: '=Bearer {{$credentials.apiKey}}',
-			},
-		},
-	};
+  name = 'gleanClientApi';
+  displayName = 'Glean Client API';
+  // Uses the link to this tutorial as an example
+  // Replace with your own docs links when building your own nodes
+  documentationUrl = 'https://developers.glean.com/api-info/client/getting-started/overview';
+  properties: INodeProperties[] = [
+    {
+      displayName: 'Base URL',
+      name: 'baseUrl',
+      type: 'string',
+      default: 'https://support-lab-be.glean.com',
+    },
+    {
+      displayName: 'API Key',
+      name: 'apiKey',
+      type: 'string',
+      typeOptions: { password: true },
+      default: '',
+    },
+  ];
+  authenticate: IAuthenticateGeneric = {
+    type: 'generic',
+    properties: {
+      headers: {
+        Authorization: '=Bearer {{$credentials.apiKey}}',
+      },
+    },
+  };
 
-	test: ICredentialTestRequest = {
-		request: {
-			baseURL: '={{$credentials.baseUrl}}',
-			url: '/rest/api/v1/search',
-			method: 'POST',
-		},
-	};
+  test: ICredentialTestRequest = {
+    request: {
+      baseURL: '={{$credentials.baseUrl}}',
+      url: '/rest/api/v1/search',
+      method: 'POST',
+    },
+  };
 }
